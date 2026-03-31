780619c99def44390a293ef8c5065b11a101ce9b
GS1-HUB <2241539@my.ipleiria.pt>
2026-03-31 22:02:36 +0100

    #3-StockFrom .Add the method to create Products

---
+++ SimpleStockApp/Models/Produtcs.vb
@@ -0,0 +1,13 @@
+﻿Imports System.ComponentModel.DataAnnotations.Schema
+Imports System.Windows.Documents
+<Table("Products")>
+Public Class Produtcs
+
+    Public Property Id As Integer
+    Public Property Name As String
+    Public Property Price As Integer
+    Public Property Descont As Integer
+    Public Property Stock As Integer
+    Public Property Company_Id As Integer
+
+End Class
